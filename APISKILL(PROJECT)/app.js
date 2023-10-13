752b06e610bce95dee2968e32fe51e63cd0be0de
Muthukumaran-VD <142777400+Muthukumaran-VD@users.noreply.github.com>
2023-10-13 10:53:47 +0530

    Add files via upload

--- APISKILL(PROJECT)/app.js
+++ APISKILL(PROJECT)/app.js
@@ -3,19 +3,21 @@ const app = express();
 const port = 3000;
 const module1 = require('./models/Skill');
 
+
 app.get('/api/technical-skills', async (req, res) => {
   try {
-    let database = await module1.getDatabase();
-    console.log("DB Connected")
+    const database = await module1.getDatabase();
+    console.log("DB Connected");
     const collection = database.collection('technicalskills');
-    const cursor = collection.find({});
-    let skill = await cursor.toArray();
-    res.json(skill);
+    const skills = await collection.find({}, { _id: 0, skill: 1 }).toArray();
+    const skillArray = skills.map(item => item.skill);
+    res.json(skillArray);
   } catch (error) {
     console.error(error);
     res.status(500).send('Error retrieving data');
   }
 });
+
 
 var server = app.listen(port, function() {
   console.log("Listening on port %d", server.address().port);
